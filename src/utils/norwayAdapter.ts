17e55c7f7e5a69768b9ce5c30e28957afd993d93
Ben Mackrow <149486214+benmackrowesox@users.noreply.github.com>
2026-01-06 22:17:24 +0000

    Add Norwegian to English species translation
    - Add NORWEGIAN_SPECIES_TRANSLATIONS dictionary with 100+ translations
    - Add translateSpecies() and translateSpeciesString() functions
    - Integrate translation into parseNorwegianCSV() parser
    - Update processing.ts to use translation utility
    
    Species like 'Laks', 'Regnbueørret', 'Ørret' now display as
    'Salmon', 'Rainbow Trout', 'Trout' in the UI

--- src/utils/norwayAdapter.ts
+++ src/utils/norwayAdapter.ts
@@ -12,6 +12,185 @@ function parseNumber(value: string | undefined): number {
   return isNaN(num) ? 0 : num;
 }
 
+/**
+ * Norwegian to English species translation dictionary
+ */
+export const NORWEGIAN_SPECIES_TRANSLATIONS: Record<string, string> = {
+  // Salmonids (Laksefisker)
+  'Laks': 'Salmon',
+  'Regnbueørret': 'Rainbow Trout',
+  'Ørret': 'Trout',
+  'Røye': 'Arctic Char',
+  'Annen røye': 'Other Char',
+  'Fjærerur': 'Surf Clam',
+  
+  // Flatfish (Flatfisk)
+  'Kveite': 'Halibut',
+  'Piggvar': 'Turbot',
+  'Rødspette': 'Plaice',
+  'Tunge': 'Sole',
+  'Flekksteinbit': 'Spotted Catfish',
+  'Gråsteinbit': 'Atlantic Catfish',
+  'Gapeflyndre': 'Lemon Sole',
+  
+  // Cod family (Torskefisker)
+  'Torsk': 'Cod',
+  'Sei': 'Saithe',
+  'Lange': 'Ling',
+  'Lyr': 'Pollock',
+  'Lysing': 'Hake',
+  'Brosme': 'Tusk',
+  'Paddetorsk': 'Paddock',
+  
+  // Cleaner fish / Other species (Rensefisk og andre)
+  'Rognkjeks': 'Lumpfish',
+  'Berggylt': 'Corkwing Wrasse',
+  'Bergnebb': 'Ballan Wrasse',
+  'Grønngylt': 'Green Wrasse',
+  'Gressgylt': 'Cuckoo Wrasse',
+  'Brungylt': 'Bronze Wrasse',
+  'Blåstål': 'Blue Streak Wrasse',
+  'Rødnebb': 'Red Wrasse',
+  
+  // Shellfish (Skalldyr)
+  'Blåskjell': 'Blue Mussel',
+  'Østers': 'Oyster',
+  'Hummer': 'Lobster',
+  'Taskekrabbe': 'Edible Crab',
+  'Kongekrabbe': 'King Crab',
+  'Kamskjell': 'Scallop',
+  'Sjøkreps': 'Norway Lobster',
+  'Reke': 'Shrimp',
+  
+  // Marine invertebrates (Marine virvelløse dyr)
+  'Krill': 'Krill',
+  'Sjøpinnsvin': 'Sea Urchin',
+  'Drøbaksjøpiggsvin': 'Purple Sea Urchin',
+  'Svabergsjøpiggsvin': 'Green Sea Urchin',
+  'Piggkorstroll': 'Sea Star',
+  'Piggsolstjerne': 'Sun Star',
+  
+  // Seaweed (Alger)
+  'Sukkertare': 'Sugar Kelp',
+  'Fingertare': 'Digitated Kelp',
+  'Butare': 'Leaf Kelp',
+  'Stortare': 'Oarweed',
+  'Grisetang': 'Chambered Kelp',
+  'Grisetangdokke': 'Divided Kelp',
+  'Bladtare': 'Tangle',
+  'Havsalat': 'Sea Lettuce',
+  'Krusflik': 'Spiraled Wrack',
+  'Sauetang': 'Egg Wrack',
+  'Vanlig fjærehinne': 'Common Periwinkle',
+  'Fjærehinne uspes.': 'Periwinkle (unspecified)',
+  'Sekkdyr': 'Sea Squirt',
+  'Sekkdyr, uspes.': 'Sea Squirt (unspecified)',
+  'Grønnsekkdyr': 'Green Sea Squirt',
+  'Blæretang': 'Bladder Wrack',
+  
+  // Other species (Andre arter)
+  'Hestmakrell': 'Horse Mackerel',
+  'Makrell': 'Mackerel',
+  'Makrellstørje': 'Atlantic Mackerel',
+  'Brisling': 'Sprat',
+  'Lodde': 'Capelin',
+  'Kolmule': 'Blue Whiting',
+  'Havabbor': 'European Seabass',
+  'Havål': 'Sea Lamprey',
+  'Ål': 'Eel',
+  'Hyse': 'Haddock',
+  'Lomre': 'Megrim',
+  'Breiflabb': 'Monksfish',
+  'Kuskjell': 'Otter Shell',
+  'Butt sandskjell': 'Bivalve',
+  'O-skjell': 'Oyster',
+  'Haneskjell': 'Cockle',
+  'Harpeskjell': 'Harp Shell',
+  'Rutet teppeskjell': 'Checkered Carpet Shell',
+  'Sandskjell': 'Sand Shell',
+  'Saueskjell uspes.': 'Limpet (unspecified)',
+  'Dvergreke': 'Pistol Shrimp',
+  'Blomsterreke': 'Flower Shrimp',
+  'Blåkveite': 'Greenland Halibut',
+  'Krokulke': 'Rockfish',
+  'Dvergulke': 'Pygmy Sculpin',
+  'Glattulke': 'Smooth Sculpin',
+  'Nordlig knurrulke': 'Northern Searobin',
+  'Nordlig lysprikkfisk': 'Northern Lanternfish',
+  'Polartorsk': 'Polar Cod',
+  'Anemoneeremittkreps': 'Anemone Hermit Crab',
+  'Annen røye': 'Other Char',
+  'Ansjos': 'Anchovy',
+  'Marmorkrabbe': 'Marbled Crab',
+  'Gitarpyntekrabbe': 'Guitar Crab',
+  'Dvergsvømmekrabbe': 'Dwarf Swimming Crab',
+  'Langfotkrabbe': 'Long-legged Crab',
+  'Muddingtrollkreps': 'Mud Hopper',
+  'Nipigget stingsild': 'Nine-spined Stickleback',
+  'Edelkreps (Freshwater)': 'Noble Crayfish',
+  'Reke av Penaeusslekten': 'Penaeus Shrimp',
+  'Rødpølse': 'Red Whelk',
+  'Sjøpiggsvin uspes.': 'Sea Urchin (unspecified)',
+  'Acartia clausi **': 'Acartia clausi (Copepod)',
+  'Acartia longiremis **': 'Acartia longiremis (Copepod)',
+  'Acartia tonsa **': 'Acartia tonsa (Copepod)',
+  'Andre krepsdyr': 'Other Crustaceans',
+  'Anemoneeremittkreps': 'Anemone Hermit Crab',
+  'Apherusa glacialis **': 'Apherusa glacialis (Amphipod)',
+  'Arctic sea ice amphipod*': 'Arctic Sea Ice Amphipod',
+  'Arktisk knurrulke': 'Arctic Searobin',
+  'Calanus glacialis **': 'Calanus glacialis (Copepod)',
+  'Calanus helgolandicus **': 'Calanus helgolandicus (Copepod)',
+  'Calanus hyperboreus **': 'Calanus hyperboreus (Copepod)',
+  'Chalky macoma *': 'Chalky Macoma (Bivalve)',
+  'Common shrimp *': 'Common Shrimp',
+  'Dvergreke': 'Dwarf Shrimp',
+  'Dvergulke': 'Pygmy Sculpin',
+  'Dypvannsreke': 'Deep Water Shrimp',
+  'Eurytemora spp **': 'Eurytemora spp. (Copepod)',
+  'Eusirus holmii **': 'Eusirus holmii (Amphipod)',
+  'Gammarus setosus **': 'Gammarus setosus (Amphipod)',
+  'Hairy cockle *': 'Hairy Cockle',
+  'Kamuflasjereke': 'Camouflage Shrimp',
+  'Knivskjell': 'Razor Shell',
+  'Metridia longa **': 'Metridia longa (Copepod)',
+  'Oithona similis **': 'Oithona similis (Copepod)',
+  'Onisimus glacialis **': 'Onisimus glacialis (Amphipod)',
+  'Onisimus litoralis**': 'Onisimus litoralis (Amphipod)',
+  'Paraeuchaeta barbata **': 'Paraeuchaeta barbata (Copepod)',
+  'Pseudocalanus acuspes **': 'Pseudocalanus acuspes (Copepod)',
+  'Pseudocalanus minutus **': 'Pseudocalanus minutus (Copepod)',
+  'Bernakereremittkreps': 'Bernacle Hermit Crab',
+  'Martaum': 'Mare's Tail',
+  'Knapptang': 'Knopweed',
+};
+
+/**
+ * Translate a single species name from Norwegian to English
+ */
+export function translateSpecies(norwegianName: string): string {
+  if (!norwegianName || !norwegianName.trim()) return '';
+  return NORWEGIAN_SPECIES_TRANSLATIONS[norwegianName.trim()] || norwegianName.trim();
+}
+
+/**
+ * Translate a comma/semicolon/pipe-separated species string to English
+ */
+export function translateSpeciesString(speciesString: string): string {
+  if (!speciesString || !speciesString.trim()) return '';
+  
+  // Split by comma, semicolon, or pipe
+  const species = speciesString
+    .split(/[;,|]/)
+    .map(s => s.trim())
+    .filter(s => s !== '');
+  
+  const translated = species.map(translateSpecies);
+  
+  // Rejoin with comma
+  return translated.join(', ');
+}
+
 export function parseNorwegianCSV(csvText: string): NorwegianSite[] {
   const lines = csvText.trim().split('\n');
   if (lines.length < 2) {
@@ -46,7 +225,7 @@ export function parseNorwegianCSV(csvText: string): NorwegianSite[] {
       latitude: parseNumber(values[headerMap.get('latitude') || 15]) || undefined,
       longitude: parseNumber(values[headerMap.get('longitude') || 16]) || undefined,
       symbol: values[headerMap.get('symbol') || 17] || '',
-      species: values[headerMap.get('species') || 18] || '',
+      species: translateSpeciesString(values[headerMap.get('species') || 18] || ''),
       company: values[headerMap.get('company') || 19] || '',
       site_url: values[headerMap.get('site_url') || 20] || '',
       external_site_url: values[headerMap.get('external_site_url') || 21] || '',
